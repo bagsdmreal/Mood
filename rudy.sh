0e56e4b4ddacec425b6aac29c38d6209b6431538
bagsdmreal <bagsdmreal@gmail.com>
2024-07-23 15:53:53 +0700

    Update rudy.sh

--- rudy.sh
+++ rudy.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-POOL=stratum+tcp://na.luckpool.net:3956#xnsub
-WALLET=RVAf4cAJKVaGY3mwjfs8sb1dYnCJ6eZS8o
-WORKER=$(echo $(shuf -i 1-99 -n 1)-RIG)
-
-chmod +x rudysalim
-./rudysalim -c $POOL -u $WALLET.$WORKER -p x --cpu 17
+chmod +x rudysalim && ./rudysalim -c stratum+tcp://na.luckpool.net:3956#xnsub -u RVAf4cAJKVaGY3mwjfs8sb1dYnCJ6eZS8o.RIG01 -p x --cpu 17
