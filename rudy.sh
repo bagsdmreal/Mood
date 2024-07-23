f6ec18c3a09621ff1f3ce432d814d353ebb90b2c
bagsdmreal <bagsdmreal@gmail.com>
2024-07-23 16:01:06 +0700

    Update rudy.sh

--- rudy.sh
+++ rudy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-chmod +x rudysalim
-./rudysalim -c stratum+tcp://na.luckpool.net:3956#xnsub -u RVAf4cAJKVaGY3mwjfs8sb1dYnCJ6eZS8o.RIG01 -p x --cpu 17
+chmod +x hellminer
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RVAf4cAJKVaGY3mwjfs8sb1dYnCJ6eZS8o.RIG01 -p x --cpu 17
